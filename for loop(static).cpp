f3aadbcfd768639dc444e2ee9329eefb608e64f2
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-11-27 18:14:36 +0530

    Add files via upload

---
+++ for loop(static).cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=100,b=120;
+		for (a=100;a<123;a++)
+		{
+	printf("\n the value of a after incremented is: %d",a);
+}
+for (b=120;b>99;b--)
+{
+	printf("\n the value of b after decremented is: %d",b);
+}
+	return 0;
+}

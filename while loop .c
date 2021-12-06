897d3a9a65290d335006bce0a18c2477ea7d55c9
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-06 15:44:26 +0530

    Create while loop .c

---
+++ while loop .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    while(a<n)
+    {
+    printf("\n%d",a);
+    ++a;
+    }
+    return 0;
+}

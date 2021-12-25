20037d4e2e7ebee5d45f43736a88ac75307056f9
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-25 20:59:20 +0530

    Update module 2(finding min among three numbers) .c

--- module 2(finding min among three numbers) .c
+++ module 2(finding min among three numbers) .c
@@ -4,16 +4,16 @@ int main()
     int a,b,c;
     printf("enter the values of three numbers: ");
     scanf("%d %d %d",&a,&b,&c);
-    if(a<b&&b<c)
+    if(a<b&&a<c)
     {
         printf("%d is smaller than %d and %d",a,b,c);
     }
-    else if(b<c)
+    else if(b<a&&b<c)
     {
-        printf("%d is smaller than %d",b,c);
+        printf("%d is smaller than %d and %d",b,a,c);
     }
     else
     {
-        printf("%d is smallest",c);
+        printf("%d is smaller than %d and %d",c,a,b);
     }
 }

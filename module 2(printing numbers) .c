87fbee99267070f6543392846c6275d7b266c733
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-30 09:52:39 +0530

    Create module 2(printing numbers) .c

---
+++ module 2(printing numbers) .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a=1;
+    for(j=1;j<5;j++)
+    {
+        for(i=1;i<=j;i++)
+        {
+            printf("%d",a);
+            a++;
+            
+        }
+            
+            {
+                printf("\n");
+        }
+    }
+}

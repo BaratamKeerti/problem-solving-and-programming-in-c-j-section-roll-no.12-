0805b2044ef2fd5ce549373eb9c75a972733be71
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-31 19:20:27 +0530

    Create assignment 2(finding cube root of a number) .c

---
+++ assignment 2(finding cube root of a number) .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,cbrt;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    cbrt=pow(a,0.3334);
+    printf("the cube root of %d is %d",a,cbrt);
+}

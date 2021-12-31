01ca91700a43e78b0bdc7e4da7bb608e6cef8381
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-31 19:23:40 +0530

    Create assignment 2(printing numbers in reverse order) .c

---
+++ assignment 2(printing numbers in reverse order) .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    printf("enter the value of a: ");
+    scanf("%d",&a);
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    for(a;a>n;a--)
+    {
+        printf("%d  ",a);
+    }
+}

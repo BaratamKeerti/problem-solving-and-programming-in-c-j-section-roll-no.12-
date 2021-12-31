5cb7085a62acc9075f42539cb0c8cf561e5a2cdc
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-31 19:46:09 +0530

    Create assignment 2(conversion of mass from kg to ounce).c

---
+++ assignment 2(conversion of mass from kg to ounce).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int kg;
+    float ounce;
+    printf("enter the value of mass in kilograms: ");
+    scanf("%d",&kg);
+    ounce=kg*35.274;
+    printf("mass in ounce is %f",ounce);
+}

d9702ac53a3d6293de7472468f6904dca702c4c0
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2021-12-31 19:50:08 +0530

    Update assignment 2(finding factorial of a number).c

--- assignment 2(finding factorial of a number).c
+++ assignment 2(finding factorial of a number).c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int i=1,number,factorial=char16_t;
+int i=1,number,factorial=1;
 printf("enter the number whoose factorial you want to find: ");
 scanf("%d",&number);
 for(i=1;i<=number;i++)

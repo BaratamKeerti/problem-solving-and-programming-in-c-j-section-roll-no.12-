bbc3ca049d8cacfd07ba042be384c4c01497a929
BaratamKeerti <93912794+BaratamKeerti@users.noreply.github.com>
2022-03-14 23:59:03 +0530

    Create module 4(Find transpose of a matrix).c

---
+++ module 4(Find transpose of a matrix).c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],i,j,m,n,b[10][10];
+    printf("enter the number of rows and columns:");
+    scanf("%d %d",&m,&n);
+    printf("enter the values:");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            b[j][i]=a[i][j];
+        }
+        printf("\n");
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+}
+
